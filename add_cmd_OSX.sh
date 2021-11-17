4fac25609c89c5478f0c28976931d649cf5fb11b
seungjinyu <seungjinyu93@gmail.com>
2021-11-17 11:39:16 +0900

    prototype

--- add_cmd_OSX.sh
+++ add_cmd_OSX.sh
@@ -1,3 +1,8 @@
+echo "building gitcode binary file"
 go build . 
-cp gitcode /usr/local/bin
+
+echo "adding cmd to /usr/local/bin"
+cp -av gitcode /usr/local/bin
+
+echo "removing gitcode binary on repo"
 rm gitcode
