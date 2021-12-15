fec90c410977f9e0f595658bc1c7a4965b3ef60c
seungjinyu <seungjinyu93@gmail.com>
2021-12-15 14:15:09 +0900

    [ADD] cobra command and a lots of command for gitcode version 0.1.0

--- main.go
+++ main.go
@@ -1,81 +1,20 @@
+/*
+Copyright © 2021 NAME HERE <EMAIL ADDRESS>
+
+*/
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"log"
-	"os"
-	"os/exec"
-	"runtime"
-	"strings"
+
+	"github.com/joho/godotenv"
+	"github.com/seungjinyu/gitcode/cmd"
 )
 
 func main() {
-	fp, err := os.Open("./.git/config")
+	err := godotenv.Load(".env")
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("env file import error")
 	}
-	defer fp.Close()
-
-	// buffer := make([]byte, 10)
-
-	// r := bufio.NewReader(fp)
-	// b, err := r.Peek(1000)
-	// if err != nil {
-	// 	fmt.Println(err)
-	// }
-	// fmt.Printf("5 bytes: %s\n", string(b))
-
-	// Splits on newlines by default.
-	res, err := findString(fp, "url")
-
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	giturl := splitString(res)
-	fmt.Println(giturl)
-
-	openBrowser(giturl)
-
-}
-func openBrowser(url string) {
-	var err error
-	switch runtime.GOOS {
-	case "darwin":
-		err = exec.Command("open", url).Start()
-
-	default:
-		err = fmt.Errorf("unsuppored platform")
-	}
-	if err != nil {
-		log.Fatal(err)
-	}
-
-}
-
-func splitString(res string) string {
-	slice := strings.Split(res, " ")
-	return slice[2]
-}
-
-func findString(fp *os.File, str string) (string, error) {
-	scanner := bufio.NewScanner(fp)
-
-	line := 1
-	// https://golang.org/pkg/bufio/#Scanner.Scan
-	for scanner.Scan() {
-		if strings.Contains(scanner.Text(), str) {
-			resultStr := scanner.Text()
-			return resultStr, nil
-		}
-		line++
-	}
-
-	if err := scanner.Err(); err != nil {
-		// Handle the error
-		fmt.Println(err)
-	}
-
-	return "Nothing", nil
+	cmd.Execute()
 }
