3efd6e8ced062c1787a7cd3a4bcbad6a1b07daef
seungjinyu <seungjinyu93@gmail.com>
2021-12-15 17:49:15 +0900

    [FIX] fixed error printing and used return

--- phase/show.go
+++ phase/show.go
@@ -18,8 +18,8 @@ func Show(args []string) {
 
 	fp, err := os.Open(os.Getenv("GITPATH"))
 	if err != nil {
-		fmt.Println("failed to open .git file move to the directory where .git is located : ) ")
-		// fmt.Println(err)
+		fmt.Println("[ERROR] failed to open .git file move to the directory where .git is located : ) ")
+		return
 	}
 	defer fp.Close()
 
