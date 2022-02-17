27b5d1f492240055e3b77175a915e98e252536d7
whitegoblin <seungjinyu93@gmail.com>
2022-02-17 14:32:36 +0900

    [ADD] added explanation about th commands

--- cmd/show.go
+++ cmd/show.go
@@ -12,13 +12,8 @@ import (
 // showCmd represents the show command
 var showCmd = &cobra.Command{
 	Use:   "show",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Show commands shows the github repo on your browser immediately",
+	Long:  `Show uses the .git folder and finds out the url and opens it on the browser`,
 	Run: func(cmd *cobra.Command, args []string) {
 		phase.Show(args)
 	},
@@ -26,14 +21,4 @@ to quickly create a Cobra application.`,
 
 func init() {
 	rootCmd.AddCommand(showCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// showCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// showCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
--- main.go
+++ main.go
@@ -5,10 +5,12 @@ Copyright © 2021 NAME HERE <EMAIL ADDRESS>
 package main
 
 import (
+	"os"
+
 	"github.com/seungjinyu/gitcode/cmd"
 )
 
 func main() {
-
+	os.Setenv("Version", "1.0.0")
 	cmd.Execute()
 }
